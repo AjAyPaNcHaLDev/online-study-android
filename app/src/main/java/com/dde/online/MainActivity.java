2f069fc0a39099e494388a6715ce720c746e4239
ajaypanchaldev <ap8570091@gmail.com>
2022-08-30 17:31:19 +0530

    nav bar added

--- app/src/main/java/com/dde/online/MainActivity.java
+++ app/src/main/java/com/dde/online/MainActivity.java
@@ -2,22 +2,29 @@ package com.dde.online;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.dde.online.Fragment.Home;
 import com.dde.online.Fragment.MyProfile;
 import com.dde.online.utils.Tools;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView navigation;
     private ActionBar actionBar;
+
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +32,39 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.containerFrameLayout,new Home()).commit();//    this is the home fragment by default
         initComponent();
         initToolbar();
+        initNavigationMenu();
     }
 
 
+
     private void initToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         actionBar = getSupportActionBar();
         actionBar.setLogo(R.drawable.dde_logo);
-        actionBar.setTitle("");
-//        actionBar.setDisplayHomeAsUpEnabled(true);
-        Tools.setSystemBarColor(this, R.color.grey_1000);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle(" ");
+        Tools.setSystemBarColor(this, R.color.grey_10);
+        Tools.setSystemBarLight(this);
+    }
+
+
+
+
+    private void initNavigationMenu() {
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        // open drawer at start
+        drawer.openDrawer(GravityCompat.START);
+   drawer.close();
 
     }
     private void initComponent() {
